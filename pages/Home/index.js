aff982eda627bbae70374a41035f77b7afc7dc1d
ana-111 <anacarolinaamaralsilva2003@gmail.com>
2020-11-17 11:13:46 -0300

    Autenticação pelo Nyous finalizada

---
+++ pages/Home/index.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, FlatList, Text, View } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import ItemEvento from '../../components/ItemEvento';
+
+const Home = () => {
+
+    //const [token, setToken] = useState('');
+
+   // const getToken = async () => {
+   //     setToken(await AsyncStorage.getItem('@jwt'));
+   // }
+
+   // useEffect(()=>{
+   //     getToken();
+   // }, [])
+    const [evento, setEventos] = useState([]);
+
+    useEffect(() => 
+    {
+        listarEventos();
+    },[])
+
+    const listarEventos = () => {
+        fetch(`http://192.168.3.55:5000/api/eventos`)
+        .then(response => response.json())
+        .then(dados => {
+            setEventos(dados.data);
+            console.log(dados.data);
+        })
+        .catch(err => console.error(err));
+    }
+
+    const renderItem = (evento) => {
+        return (
+            <ItemEvento 
+                nome={evento.item.nome} 
+                imagem={evento.item.urlImagem}
+                link={evento.item.link} />
+        )
+    }   
+    
+
+    return(
+        <View>
+            <Text>HOME</Text>
+            {/* <Text>{token}</Text>*/}
+            <FlatList 
+                data={eventos}
+                keyExtractor={item => item.id}
+                renderItem={renderItem}
+            />
+        </View>
+    )
+}
+
+export default Home;
---
+++ pages/Login/index.js
@@ -0,0 +1,110 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Login = ( {navigation} ) => {
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const salvar = async (value) => {
+        try {
+          await AsyncStorage.setItem('@jwt', value)
+        } catch (e) {
+          // saving error
+        }
+    }
+
+    const Logar = () => {
+        
+        const corpo = {
+            email : email,
+            senha : senha
+        }
+
+        fetch('http://192.168.3.55:5000/api/Account/login', {
+            method: 'POST',
+            headers :{
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify(corpo)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if(data.status != 404){
+                alert('Seja bem vindx');
+                console.log(data.token);
+                
+                salvar(data.token);
+                navigation.navigate('Autenticado');
+            }else{
+                alert('Email ou senha inválidos! :( ');
+            }
+        })
+
+    }
+
+    return (
+    
+        <View style={styles.container}>
+             <TextInput
+               style={styles.input}
+                onChangeText={text => setEmail(text)}
+                value={email}
+                placeholder="Digite seu email"
+   />
+     <TextInput
+               style={styles.input}
+                onChangeText={text => setSenha(text)}
+                value={senha}
+                placeholder="Digite sua Senha"
+                secureTextEntry={true}
+   />
+    <TouchableOpacity
+       style={styles.button}
+       onPress={Logar}
+     >
+       <Text style={styles.textbutton}>Entrar</Text>
+     </TouchableOpacity>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ffff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input : {
+        width: '90%',
+        height: 40, 
+        borderColor: 'gray', 
+        borderWidth: 1,
+        marginTop : 20,
+        padding: 5,
+        borderRadius: 6
+    },
+    button : {
+        backgroundColor : 'black',
+        width: '90%',
+        padding : 10,
+        borderRadius: 6,
+        marginTop : 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textButton : {
+        color : 'white'
+    },
+    logo : {
+        width: 200,
+        height: 200,
+    }
+  });
+
+export default Login;
